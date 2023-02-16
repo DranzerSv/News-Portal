7caddaf18ef2c7803d5e8793d3bbf9c692017a01
Douglas Navarrete <dnavarrete@applaudostudios.dev>
2023-02-16 08:23:44 -0600

    feature: on utils.js created buildRequest, objectsToParams, setDefaultFilters

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const apiKey = 'api-key=4a997527-1fe8-435b-8f5f-bc405e8b6954';
+let filters = localStorage.getItem('savedFilters');
+
+if (!filters) {
+  filters = setDefaultFilters();
+}
+let request = buildRequest(filters, apiKey);
+
+console.log(request);
---
+++ js/utils.js
@@ -0,0 +1,29 @@
+function setDefaultFilters() {
+  return {
+    query: 'bleach',
+    language: null,
+    pageSize: 10,
+  };
+}
+function buildRequest(filters, apiKey) {
+  let parameters = objectsToParams(filters);
+
+  return `https://content.guardianapis.com/search?${parameters}&show-fields=thumbnail,headline&${apiKey}`;
+}
+
+function objectsToParams(filters) {
+  let parameters = '';
+  if (filters['query']) {
+    parameters += 'q=' + filters['query'];
+  }
+  if (filters['language']) {
+    parameters += '&lang=' + filters['language'];
+  }
+  if (filters['pageSize'] !== 10) {
+    parameters += '&page-size=' + filters['pageSize'];
+  } else {
+    parameters += '&page-size=10';
+  }
+
+  return parameters;
+}
